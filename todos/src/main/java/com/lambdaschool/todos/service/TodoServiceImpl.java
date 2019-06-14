3bf7e732ac4767b647f1a473b72c30128da4f23a
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-06-14 10:50:01 -0700

    need to make a repository to continue

--- todos/src/main/java/com/lambdaschool/todos/service/TodoServiceImpl.java
+++ todos/src/main/java/com/lambdaschool/todos/service/TodoServiceImpl.java
@@ -1,13 +1,20 @@
 package com.lambdaschool.todos.service;
 
 import com.lambdaschool.todos.model.Todo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 
 public class TodoServiceImpl implements TodoService{
+
+    private static final Logger logger = LoggerFactory.getLogger(TodoServiceImpl.class);
+
     @Override
     public ArrayList<Todo> findAll() {
-        return null;
+        ArrayList<Todo> list = new ArrayList<>();
+        Todo.findAll().iterator().forEachRemaining(list::add);
+        return list;
     }
 
     @Override
