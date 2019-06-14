d0c9e0a231285f59d0ee4619ae282cc6abbcbd7f
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-06-14 13:43:48 -0700

    generated code

--- todos/src/main/java/com/lambdaschool/todos/service/TodoServiceImpl.java
+++ todos/src/main/java/com/lambdaschool/todos/service/TodoServiceImpl.java
@@ -1,5 +1,35 @@
 package com.lambdaschool.todos.service;
 
-public class TodoServiceImpl {
+import com.lambdaschool.todos.model.Todo;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service(value = "todoservice")
+public class TodoServiceImpl implements TodoService {
+
+    @Override
+    public List<Todo> findAllById(long id) {
+        return null;
+    }
+
+    @Override
+    public Todo findTodoById(long id) {
+        return null;
+    }
+
+    @Override
+    public Todo save(Todo todo) {
+        return null;
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
+
+    @Override
+    public Todo update(Todo todo, long id) {
+        return null;
+    }
 }
