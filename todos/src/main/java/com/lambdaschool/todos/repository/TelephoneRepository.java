e8ebc3b903d9cc8821b61e41d5666e8e46444e4f
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-06-14 09:18:23 -0700

    Added repository

---
+++ todos/src/main/java/com/lambdaschool/todos/repository/TelephoneRepository.java
@@ -0,0 +1,8 @@
+package com.lambdaschool.todos.repository;
+
+import com.lambdaschool.zoos.model.Telephone;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TelephoneRepository extends CrudRepository<Telephone, Long>
+{
+}
---
+++ todos/src/main/java/com/lambdaschool/todos/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.lambdaschool.todos.repository;
+
+import com.lambdaschool.zoos.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long>
+{
+    User findByUsername(String username);
+}
